17c6efac782337a11255a2a350b66013284862fc
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2013-05-03 03:38:00 -0300

    Testing the file handlers with the brute.c file

--- PAA/paradigms/src/brute.c
+++ PAA/paradigms/src/brute.c
@@ -4,6 +4,20 @@
 
 int main(int argc, char argv[])
 {
-    printf("hello nurse!");
+    FILE file = open_file("inputs/00_small_input");
+    printf("file: %d\n", file);
+
+    int min_seq_size = get_seq_size(&file);
+
+    while(min_seq_size != 0)
+    {
+        Word word0 = word_handler(&file);
+        Word word1 = word_handler(&file);
+
+        min_seq_size = get_seq_size(&file);
+
+        printf("k: %d\nword0: %s\nword1: %s\n", min_seq_size, word0.letters, word1.letters);
+    }
+    
     return 0;
 }
