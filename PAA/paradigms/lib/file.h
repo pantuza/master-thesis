382a45962f883b90bb3895b0a5f4db8f61190515
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2013-05-03 03:36:48 -0300

    Created the generic file functions

--- PAA/paradigms/lib/file.h
+++ PAA/paradigms/lib/file.h
@@ -2,35 +2,18 @@
 #define FILE_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
+#include "strtype.h"
 
 #define FILENAME_SIZE 50
 #define FILE_PERMISSION "r"
 #define SUCCESS 0
 
 
-FILE open_file(char filename[FILENAME_SIZE])
-{
-    FILE *file = fopen(filename, FILE_PERMISSION);
-
-    if(file == NULL)
-    {
-        perror("Error during file open");
-        exit(1);
-    }
-
-    return *file;
-}
-
-void close_file(FILE **file)
-{
-    int status = fclose(file);
-
-    if(status != SUCCESS)
-    {
-        printf("Error during file close. FILE: %d\n", file);
-        exit(2);
-    }
-}
+FILE open_file(char filename[FILENAME_SIZE]);
+int get_seq_size(FILE *file);
+Word word_handler(FILE *file);
+void close_file(FILE *file);
 
 #endif
