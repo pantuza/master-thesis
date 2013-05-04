53ef886723348153007b4c9ad65a668c25b0ab73
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2013-05-04 00:02:50 -0300

    included file.h to enable stdlib and file functions

--- PAA/paradigms/src/brute.c
+++ PAA/paradigms/src/brute.c
@@ -4,20 +4,20 @@
 
 int main(int argc, char argv[])
 {
-    FILE file = open_file("inputs/00_small_input");
-    printf("file: %d\n", file);
+    FILE *file = open_file("inputs/00_small_input");
+    printf("file: %d\n\n", file);
 
-    int min_seq_size = get_seq_size(&file);
+    int min_seq_size = get_seq_size(file);
 
     while(min_seq_size != 0)
     {
-        Word word0 = word_handler(&file);
-        Word word1 = word_handler(&file);
+        Word word0 = word_handler(file);
+        Word word1 = word_handler(file);
 
-        min_seq_size = get_seq_size(&file);
-
-        printf("k: %d\nword0: %s\nword1: %s\n", min_seq_size, word0.letters, word1.letters);
+        printf("k: %d\nword0: %sword1: %s\n", min_seq_size, word0.letters, word1.letters);
+        min_seq_size = get_seq_size(file);
     }
     
+    close_file(file);
     return 0;
 }
--- PAA/paradigms/src/greedy.c
+++ PAA/paradigms/src/greedy.c
@@ -1,3 +1,4 @@
+#include "file.h"
 #include "greedy.h"
 
 
