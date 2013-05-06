5177b866d6baee1bb6018a13be460f7e84d09723
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2013-05-06 00:52:32 -0300

    changing brute force functions. @TODO: apply a recursive aproach

--- PAA/paradigms/lib/brute.h
+++ PAA/paradigms/lib/brute.h
@@ -1,2 +1,10 @@
+#ifndef BRUTE_H
+#define BRUTE_H
+
 #include "strtype.h"
 
+
+int brute_force(int segment_size, Word *word0, Word *word1);
+void sweep_input(FILE *file);
+
+#endif
--- PAA/paradigms/src/brute.c
+++ PAA/paradigms/src/brute.c
@@ -1,23 +1,57 @@
 #include "file.h"
 #include "brute.h"
+#include <math.h>
+
+
+char** alloc_subseqs(int size)
+{
+    int i = 0;
+    char **seqs = (char**) malloc(size * sizeof(char*));
+
+    for(;i < size;i++)
+        subseqs0[i] = (char**) malloc(size * sizeof(char*));
+
+    return seqs;
+}
+
+int brute_force(int segment_size, Word *word0, Word *word1)
+{
+    int i = 0;
+    int j = 0;
+
+    char **subseqs0 = alloc_subseqs(word0->length);
+
+    while(i < word0->length)
+        while(j < word0->length)
+            int k = i;
+            for(; k <= j; k++)
+    }
+}
+
+void sweep_input(FILE *file)
+{
+    int LCS;
+    int segment_size = get_seq_size(file);
+
+    while(segment_size != 0)
+    {
+        Word word0 = word_handler(file);
+        Word word1 = word_handler(file);
+
+        LCS = brute_force(segment_size, &word0, &word1);
+
+        segment_size = get_seq_size(file);
+        
+        printf("k: %d\nword0: %sword1: %sLCS: %d\n", 
+                segment_size, word0.letters, word1.letters, LCS);
+    }
+}
 
 
 int main(int argc, char argv[])
 {
     FILE *file = open_file("inputs/00_small_input");
-    printf("file: %d\n\n", file);
-
-    int min_seq_size = get_seq_size(file);
-
-    while(min_seq_size != 0)
-    {
-        Word word0 = word_handler(file);
-        Word word1 = word_handler(file);
-
-        printf("k: %d\nword0: %sword1: %s\n", min_seq_size, word0.letters, word1.letters);
-        min_seq_size = get_seq_size(file);
-    }
-    
+    sweep_input(file);
     close_file(file);
     return 0;
 }
