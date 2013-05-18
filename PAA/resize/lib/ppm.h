1ec6c639df6e54d0a1534d546cebdae9be8b7c93
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2013-05-18 01:10:23 -0300

    Defining the interface of PPM files handler

---
+++ PAA/resize/lib/ppm.h
@@ -0,0 +1,7 @@
+#ifndef PPM_H
+#define PPM_H
+
+Image import(FILE *);
+int export(Image *);
+
+#endif
---
+++ PAA/resize/src/ppm.c
@@ -0,0 +1,29 @@
+/**
+ * This is the PPM parser.
+ * 
+ * The PPM format is a lowest common denominator color image file format
+ * documentation: http://netpbm.sourceforge.net/doc/ppm.html
+ *
+ * PPM format is simpler than JPEG and PNG. It describes the image
+ * as a text file. 
+ *
+ * This file is a PPM parser to import and export PPM files.
+ *
+ * @author: Gustavo Pantuza
+ * @since: 17.05.2013
+ *
+ */
+
+#include "ppm.h"
+#include "file.h"
+
+
+Image import(FILE *file)
+{
+
+}
+
+int export(Image *image)
+{
+
+}
