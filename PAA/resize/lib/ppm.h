e9ee89ea8b015dca6de57700ddf98cbf9b452c95
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2013-05-18 05:14:46 -0300

    PPM file parser
    Created the structs to represent PPM files
    Created import function and its dependencies to handle
    the PPM files representation inside the program
    Created allocation strategy to the pixels matrix

--- PAA/resize/lib/ppm.h
+++ PAA/resize/lib/ppm.h
@@ -1,7 +1,42 @@
 #ifndef PPM_H
 #define PPM_H
 
-Image import(FILE *);
-int export(Image *);
+/* Size of magic string used in header of PPM image files */
+#define MAGIC_STRING_SIZE 3
+
+/* Token used by PPM format to identify comments inside a PPM file */
+#define COMMENTS_TOKEN '#'
+
+/* Maximum line size of a PPM file */
+#define BUFFER_SIZE 71
+
+
+/* Struct representing one pixel with its RGB values */
+typedef struct
+{
+    int R;
+    int G;
+    int B;
+} Pixel;
+
+
+/* Struct representing one Image in the PPM format */
+typedef struct
+{
+    char magic_string[MAGIC_STRING_SIZE];
+    int width;
+    int heigh;
+    int intensity;
+    Pixel **pixels;
+} PPMImage;
+
+
+/* Functions */
+void check_magic_string(FILE *, PPMImage *);
+void ignore_garbage(FILE *, char *);
+int get_number(FILE *);
+void allocate_pixels(FILE *, PPMImage *);
+PPMImage import(FILE *);
+int export(PPMImage *);
 
 #endif
--- PAA/resize/src/ppm.c
+++ PAA/resize/src/ppm.c
@@ -14,15 +14,106 @@
  *
  */
 
+#include <string.h>
+
 #include "ppm.h"
 #include "file.h"
 
 
-Image import(FILE *file)
+/**
+ * Verifies the PPM header. If the header is invalid
+ * reports an error
+ */
+void check_magic_string(FILE *file, PPMImage *image)
 {
+    fgets(image->magic_string, MAGIC_STRING_SIZE, file);
 
+    /* Plain text files must have the 'P3' magic string as a header */
+    if(image->magic_string[0] != 'P' || image->magic_string[1] != '3')
+    {
+        fprintf(stderr, "Invalid PPM file");
+        exit(EXIT_FAILURE);
+    }
 }
 
+/**
+ * Ignore the unnecessary stuff brought from the file
+ */
+void ignore_garbage(FILE *file, char *buffer) 
+{
+    while(*buffer != COMMENTS_TOKEN)
+    {
+        if(!*buffer) return;
+        buffer++;
+    }
+    while(*buffer)
+    {
+        if(*buffer == '\n') return;
+        buffer++;
+    }
+
+    int c = fgetc(file);
+    while (c && c != '\n' && c != EOF);
+        c = fgetc(file);
+}
+
+/**
+ * Get a number from file
+ */
+int get_number(FILE *file)
+{
+    char buffer[BUFFER_SIZE];
+    fgets(buffer, BUFFER_SIZE, file);
+    ignore_garbage(file, buffer);
+    return atoi(buffer);
+}
+
+/**
+ * Allocates the pixels matrix. The allocation uses malloc to create
+ * the entire storage of the matrix based in its dimensions read from
+ * the PPMImage struct. After memory allocation, it updates the pointers
+ * references and then fill the chunks with values read from file
+ */
+void allocate_pixels(FILE *file, PPMImage *image)
+{
+    int first_dimension = image.width * sizeof(int *);
+    int second_dimension = (image.width * image.height) * sizeof(Pixel);
+
+    image->pixels = (int **) malloc(first_dimension + second_dimension);
+   
+    /* First data line (modifying pointers) */
+    image.pixels[0] = image.pixels + image.width;
+
+    /* Subsequent data lines (modifying pointers) */
+    for(int i=1; i < image.width; i++)
+        image.pixels[i] = image.pixels[i-1] + image.height * sizeof(Pixel);
+
+    /* fillind the data from file */
+    for(int i = 0; i < image.width; i++)
+        for(int j = 0; j < image.height; j++)
+            image.pixels[i][j] = get_number(file);
+}
+/**
+ * Imports PPM image files and construct 
+ * PPMImage struct.
+ */
+PPMImage import(FILE *file)
+{
+    PPMImage image;
+    char buffer[BUFFER_SIZE];
+
+    check_magic_string(file, &image);
+    image.width = get_number(file);
+    image.height = get_number(file);
+    image.intensity = get_number(file);
+    allocate_pixels(file, &image);
+
+    return image;
+}
+
+/**
+ * Exports a PPMImage to a file with PPM format
+ */
 int export(Image *image)
 {
 
