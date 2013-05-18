c6bd83043fc13aade5cfc65a7f642e43658e79e7
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2013-05-18 01:09:10 -0300

    Created the main c file

---
+++ PAA/resize/src/tp2.c
@@ -0,0 +1,25 @@
+#include "file.h"
+
+void usage()
+{
+    fprintf(stderr, 
+            "Usage:\t./tp2 INPUT_FILE [OPTIONS]\n\n"
+            "INPUT_FILE\n"
+            "\tThe input file must be in ppm format. "
+            "see: http://netpbm.sourceforge.net/doc/ppm.html\n\n"
+	        "OPTIONS\n"
+            "\t-g\t\tRun the graph solution\n"
+            "\t-d\t\tRun the dynamic programming solution\n"
+            "\t-w width\tResize image in width pixels\n"
+            "\t-h height\tResize image in height pixels\n"
+            "\t-m file\t\tResize image using the file with "
+            "the weight matrices\n"
+            "--\n");
+    exit(EXIT_FAILURE);
+}
+
+int main()
+{
+    usage();
+    exit(EXIT_SUCCESS);
+}
