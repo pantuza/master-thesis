40f8b70d175ae36196eace01cf936898742f442c
{NAME} <{EMAIL}>
2013-06-04 05:33:37 -0300

    Header files of graph implementation

--- PAA/resize/lib/graph.h
+++ PAA/resize/lib/graph.h
@@ -4,6 +4,33 @@
 #include "ppm.h"
 #include "file.h"
 
+
+/* Struct of the Adjancency list  */
+typedef struct
+{
+    int vertex;
+    double energy;
+    void *next;
+} Edge;
+
+
+/* Struct of a Vertex */
+typedef struct 
+{
+    double energy;
+    Edge *edge;
+} Vertex;
+
+
+/* Struct of the entire Graph */
+typedef struct
+{
+    int list_size;
+    Vertex **vertexes;
+} Graph;
+
+
 /* Public Functions */
 void graph_resize(PPMImage *, int, int);
+Graph* init_graph(PPMImage);
 #endif
