b0c167c7cc5e431a70aea5d648eb86442ea336ca
gustavopantuza@gmail.com <gustavopantuza@gmail.com>
2013-06-13 09:27:12 -0300

    Colors preview  and options added. Debug messages removed.

--- PAA/resize/lib/debug.h
+++ PAA/resize/lib/debug.h
@@ -8,18 +8,37 @@
 #ifndef DEBUG_H_
 #define DEBUG_H_
 
+#define INFO(x) x
 #define DEBUG(x) 
 #define DEBUG1(x) 
 #define DEBUG2(x) 
-#define ASSERT(x) 
-#define ASSERT_TRUE(c,x) ASSERT(if(c){x;})
+#define ASSERT(x) x
+#define ASSERT_TRUE(c,x)  ASSERT(if(!(c)){x;})
+#define ASSERT_FALSE(c,x) ASSERT(if(c){x;})
 
 // Tuning options (Don't touch! It's too dangerous:)
 
-//#define OPT_GRAPH_IGNORE_CARVING
-// Surprisingly, this increases the runtime... Why?
+/*
+ * Don't executes Dijkstra algorithm from a start point removed in the past.
+ * Surprisingly, this increases the runtime... Why?
+ */
+#define OPT_GRAPH_IGNORE_CARVED_POINT
 
-// About 5..50% off runtime!
+/*
+ * Terminates Dijkstra algorithm if it's reach a distance bigger than
+ * the shortest path. Great results!
+ */
 #define OPT_GRAPH_SHORTEST_PATH_BREAK
+
+/*
+ * Remove vertices of shortest path from graph to perfect
+ * consecutive seam-carving.
+ * */
+#define OPT_GRAPH_REMOVE_SHORTEST_PATH
+
+/*
+ *  Paints each seam with different color
+ */
+#define OPT_IMAGE_SEAM_DIFF_COLORS
 
 #endif /* DEBUG_H_ */
--- PAA/resize/src/graph.c
+++ PAA/resize/src/graph.c
@@ -3,15 +3,33 @@
 #include "graph.h"
 #include "priority.h"
 #include "debug.h"
+#include "color.h"
 
 #define NONE  -1
-#define BREAK -2
+#define SKIP  -2
+#define BREAK -3
 
 /**
  * Convert image(x, y) to position to vet[i]
  */
-#define POS_XY(img,x,y)  ((img->height * (x)) + (y))
+#define XY2POS(img,x,y)  ((img->height * (x)) + (y))
 
+/**
+ * Convert position to vet[i] to image(x, y)
+ */
+//#define POS2XY(i,img,x,y) {x = i / img->height; y = i % img->height;}
+
+
+/**
+ * Adjacency control
+ */
+#define UP_LEFT(v,img)    ((v) - (img->height) - 1)
+#define UP_MIDDLE(v,img)  ((v) - 1)
+#define UP_RIGHT(v,img)   ((v) + (img->height) - 1)
+#define LEFT   0
+#define MIDDLE 1
+#define RIGHT  2
+#define END    3
 
 /**
  * Creates a graph based on the image pixels and fill the values for
@@ -29,56 +47,48 @@ void init_graph(Graph *graph, PPMImage *image)
     x = 0;
     for(y = 0; y < image->height - 1; y++)
     {
-        v = POS_XY(image,x,y);
-        //fprintf(stderr,"|-POS(%d,%d): %d\n",x,y,v);
-        //fprintf(stderr,"----adj: %d %d\n", POS_XY(image,x,y+1), POS_XY(image,x+1,y+1));
+        v = XY2POS(image,x,y);
         graph->vertexes[v].pixel = &(image->pixels[y][0]);
-        graph->vertexes[v].adj[0] = POS_XY(image,x,y+1);
-        graph->vertexes[v].adj[1] = POS_XY(image,x+1,y+1);
-        graph->vertexes[v].adj[2] = NONE;
-        graph->vertexes[v].adj[3] = NONE;
+        graph->vertexes[v].adj[LEFT]   = SKIP;
+        graph->vertexes[v].adj[MIDDLE] = XY2POS(image,x,y+1);
+        graph->vertexes[v].adj[RIGHT]  = XY2POS(image,x+1,y+1);
+        graph->vertexes[v].adj[END]    = NONE;
     }
 
     /* Build the middle vertices */
     for(y = 0; y < image->height - 1; y++)
         for(x = 1; x < image->width - 1; x++)
         {
-            v = POS_XY(image,x,y);
-            //fprintf(stderr,"--POS(%d,%d): %d\n",x,y,v);
-            //fprintf(stderr,"----adj: %d %d %d\n",POS_XY(image,x-1,y+1), POS_XY(image,x,y+1), POS_XY(image,x+1,y+1));
+            v = XY2POS(image,x,y);
             graph->vertexes[v].pixel = &(image->pixels[y][x]);
-            graph->vertexes[v].adj[0] = POS_XY(image,x-1,y+1);
-            graph->vertexes[v].adj[1] = POS_XY(image,x,y+1);
-            graph->vertexes[v].adj[2] = POS_XY(image,x+1,y+1);
-            graph->vertexes[v].adj[3] = NONE;
+            graph->vertexes[v].adj[LEFT]   = XY2POS(image,x-1,y+1);
+            graph->vertexes[v].adj[MIDDLE] = XY2POS(image,x,y+1);
+            graph->vertexes[v].adj[RIGHT]  = XY2POS(image,x+1,y+1);
+            graph->vertexes[v].adj[END]    = NONE;
         }
 
     /* Build the leftmost vertices */
     x = image->width - 1;
     for(y = 0; y < image->height - 1; y++)
     {
-        v = POS_XY(image,x,y);
-        //fprintf(stderr,"-|POS(%d,%d): %d\n",x,y,v);
-        //fprintf(stderr,"----adj: %d %d\n",POS_XY(image,x-1,y+1), POS_XY(image,x,y+1));
+        v = XY2POS(image,x,y);
         graph->vertexes[v].pixel = &(image->pixels[y][x]);
-        graph->vertexes[v].adj[0] = POS_XY(image,x-1,y+1);
-        graph->vertexes[v].adj[1] = POS_XY(image,x,y+1);
-        graph->vertexes[v].adj[2] = NONE;
-        graph->vertexes[v].adj[3] = NONE;
+        graph->vertexes[v].adj[LEFT]   = XY2POS(image,x-1,y+1);
+        graph->vertexes[v].adj[MIDDLE] = XY2POS(image,x,y+1);
+        graph->vertexes[v].adj[RIGHT]  = NONE;
+        graph->vertexes[v].adj[END]    = NONE;
     }
 
     /* Build the bottom vertices */
     y = image->height - 1;
     for(x = 0; x < image->width; x++)
     {
-        v = POS_XY(image,x,y);
-        //fprintf(stderr,"__POS(%d,%d): %d\n",x,y,v);
-        //fprintf(stderr,"----adj: NONE\n");
+        v = XY2POS(image,x,y);
         graph->vertexes[v].pixel = &(image->pixels[y][x]);
-        graph->vertexes[v].adj[0] = NONE;
-        graph->vertexes[v].adj[1] = NONE;
-        graph->vertexes[v].adj[2] = NONE;
-        graph->vertexes[v].adj[3] = NONE;
+        graph->vertexes[v].adj[LEFT]   = NONE;
+        graph->vertexes[v].adj[MIDDLE] = NONE;
+        graph->vertexes[v].adj[RIGHT]  = NONE;
+        graph->vertexes[v].adj[END]    = NONE;
     }
 }
 
@@ -90,6 +100,35 @@ void free_graph(Graph *graph)
     free(graph->vertexes);
 }
 
+#ifdef OPT_GRAPH_REMOVE_SHORTEST_PATH
+/**
+ * Remove a vertices from graph and adjust its adjacency
+ */
+inline static void remove_vertex(Graph *graph, PPMImage *image,
+        int vertex, int previous)
+{
+    int left   = UP_LEFT(vertex, image);
+    int middle = UP_MIDDLE(vertex, image);
+    int right  = UP_RIGHT(vertex, image);
+
+    if (previous != left && left >= 0)
+        graph->vertexes[left].adj[RIGHT] = graph->vertexes[middle].adj[RIGHT];
+
+    if (previous != right && right < graph->list_size)
+        graph->vertexes[right].adj[LEFT] = graph->vertexes[middle].adj[LEFT];
+
+    if (previous != middle)
+    {
+        if (previous == left)
+            graph->vertexes[middle].adj[MIDDLE] =
+                    graph->vertexes[left].adj[MIDDLE];
+        else //if (previous == right)
+            graph->vertexes[middle].adj[MIDDLE] =
+                    graph->vertexes[right].adj[MIDDLE];
+    }
+}
+#endif
+
 /**
  * Shortest Path solution implemented using the Dijkstra algorithm
  */
@@ -98,9 +137,8 @@ int dijkstra(Graph *graph, int source,
 {
     int vertex, adjacent, v;
     Energy alt;
-    Energy visited     = graph->limit;
+    Energy visited     = graph->limit + 4;
     Energy no_visited  = visited - 2;
-    DEBUG1(fprintf(stderr,"visited: %f, no_visite: %f\n", visited, no_visited));
 
     for(v = 0; v < graph->list_size; distance[v++] = no_visited);
     previous[source] = NONE;
@@ -108,23 +146,15 @@ int dijkstra(Graph *graph, int source,
 
     pri_queue_t priq_s;
     pri_queue priq = &priq_s;
-
     priq_init(priq, graph->list_size);
     priq_push(priq, source, graph->vertexes[source].pixel->energy);
-
-    DEBUG1(fprintf(stderr,"source: %d, distance: %f\n", source, distance[source]));
 
     int dest = NONE;
     while(priq_size(priq))
     {
         vertex = priq_pop(priq);
-
-        DEBUG1(fprintf(stderr,"exists: %d, vertex: %d, distance: %f, energy:%f \n",
-                priq_size(priq), vertex,distance[vertex],
-                graph->vertexes[vertex].pixel->energy));
         v = 0;
         adjacent = graph->vertexes[vertex].adj[v++];
-        DEBUG1(fprintf(stderr," -- adj[%d]:%d, distance: %f\n", v, adjacent, distance[adjacent]));
 
         if(adjacent == NONE)
         {
@@ -142,10 +172,10 @@ int dijkstra(Graph *graph, int source,
 
         do
         {
-            if(distance[adjacent] <= visited)
+            if(adjacent != SKIP && distance[adjacent] <= visited)
             {
-                alt = distance[vertex] + graph->vertexes[adjacent].pixel->energy;
-                DEBUG1(fprintf(stderr," -- alt: %f < %f\n", alt, distance[adjacent]));
+                alt = distance[vertex] +
+                        graph->vertexes[adjacent].pixel->energy;
                 if(alt < distance[adjacent])
                 {
                     distance[adjacent] = alt;
@@ -154,14 +184,12 @@ int dijkstra(Graph *graph, int source,
                 }
             }
             adjacent = graph->vertexes[vertex].adj[v++];
-            DEBUG1(fprintf(stderr," -- adj[%d]:%d\n", v, adjacent));
         } while(adjacent != NONE);
         distance[vertex] = visited;
     }
     priq_free(priq);
     return dest;
 }
-
 
 /**
  * Resize method that calls Dijkstra algorithm for each Vertex inside
@@ -170,13 +198,11 @@ int dijkstra(Graph *graph, int source,
 void graph_resize(PPMImage *image, int width, int height)
 {
 
-    DEBUG2(
-            for(int y = 0; y < image->height; y++) {
-                for(int x = 0; x < image->width; x++)
-                    fprintf(stderr,"%f ", image->pixels[y][x].energy);
-                fprintf(stderr,"\n");
-            }
-    );
+    if (width > image->width)
+    {
+        fprintf(stderr, "Image size too small to resize.");
+        exit(EXIT_FAILURE);
+    }
 
     Graph graph;
     init_graph(&graph, image);
@@ -187,30 +213,19 @@ void graph_resize(PPMImage *image, int width, int height)
     int *path = calloc(image->height, sizeof(int));
     Energy shortest_distance;
 
-    if (width > image->width)
-    {
-        fprintf(stderr, "Image size too small to resize.");
-        exit(EXIT_FAILURE);
-    }
+    Color color;
+    init_color(&color, width);
 
     while(width--)
     {
         shortest_distance = graph.limit + 1;
         for(x = 0; x < image->width; x++)
         {
-            i = POS_XY(image,x,0);
-            DEBUG(fprintf(stderr,"w: %d, x: %d/%dx%d, i: %d\n",
-                    width, x, image->width, image->height, i));
+            i = XY2POS(image,x,0);
 
-#ifdef OPT_GRAPH_IGNORE_CARVING
+#ifdef OPT_GRAPH_IGNORE_CARVED_POINT
             if (graph.vertexes[i].pixel->energy >= graph.energy)
-            {
-                DEBUG(fprintf(stderr,
-                        "Energy here?: %d = %f\n",
-                        i, graph.vertexes[i].pixel->energy)
-                )
                 continue;
-            }
 #endif
 
             dest = dijkstra(&graph, i, distance, previous, shortest_distance);
@@ -226,37 +241,41 @@ void graph_resize(PPMImage *image, int width, int height)
                     exit(EXIT_FAILURE);
                 }
 
-            DEBUG(fprintf(stderr,"dest: %d, distance: %f\n", dest, distance[dest]));
             if (distance[dest] < shortest_distance)
             {
                 shortest_distance = distance[dest];
-                DEBUG(fprintf(stderr, "Minimum distance = %f\n",
-                        shortest_distance));
                 y = 0;
                 while(dest != i)
                 {
-                    DEBUG(fprintf(stderr,"%d,", dest));
                     path[y++] = dest;
                     dest = previous[dest];
                 }
                 path[y++] = i;
                 // assert path length
-                ASSERT_TRUE(y != image->height,
+                ASSERT_TRUE(y == image->height,
                         fprintf(stderr,
-                                "Path length!: %d\n (must to be %d)\n",
+                                "ASSERT: Path length: %d\n (must to be %d)\n",
                                 y, image->height)
                 );
-                DEBUG(fprintf(stderr,"%d\n", dest));
             }
         }
 
         for(y = 0; y < image->height; y++)
         {
-            graph.vertexes[path[y]].pixel->R = 255;
-            graph.vertexes[path[y]].pixel->G = 0;
-            graph.vertexes[path[y]].pixel->B = 0;
+            graph.vertexes[path[y]].pixel->R = color.r;
+            graph.vertexes[path[y]].pixel->G = color.g;
+            graph.vertexes[path[y]].pixel->B = color.b;
             graph.vertexes[path[y]].pixel->energy = graph.energy;
         }
+
+#ifdef OPT_IMAGE_SEAM_DIFF_COLORS
+        next_color(&color);
+#endif
+
+#ifdef OPT_GRAPH_REMOVE_SHORTEST_PATH
+        for(y = 1; y < image->height - 1; y++)
+            remove_vertex(&graph, image, path[y], path[y-1]);
+#endif
     };
 
     free_graph(&graph);
