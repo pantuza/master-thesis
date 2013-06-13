126cf8043bcb46331fc07f1880164f8bb4bce927
{NAME} <{EMAIL}>
2013-06-12 23:18:56 -0300

    Added debug option to the code

--- PAA/resize/lib/debug.h
+++ PAA/resize/lib/debug.h
@@ -8,20 +8,18 @@
 #ifndef DEBUG_H_
 #define DEBUG_H_
 
-#define DEBUG(x)
-#define DEBUG1(x)
-#define DEBUG2(x)
-#define ASSERT(x)
+#define DEBUG(x) 
+#define DEBUG1(x) 
+#define DEBUG2(x) 
+#define ASSERT(x) 
 #define ASSERT_TRUE(c,x) ASSERT(if(c){x;})
 
 // Tuning options (Don't touch! It's too dangerous:)
 
 //#define OPT_GRAPH_IGNORE_CARVING
-// This should not happen, but really happened!
 // Surprisingly, this increases the runtime... Why?
-// Better questions: why and when this happens?
 
-// About 50% off runtime!
+// About 5..50% off runtime!
 #define OPT_GRAPH_SHORTEST_PATH_BREAK
 
 #endif /* DEBUG_H_ */
