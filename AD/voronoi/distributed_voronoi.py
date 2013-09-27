33584f7c53965371b1b441f183d15f632e64291e
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2013-09-27 14:23:33 -0300

    Modified steps of sending messages

--- AD/voronoi/distributed_voronoi.py
+++ AD/voronoi/distributed_voronoi.py
@@ -7,7 +7,8 @@ from pymote.algorithm import NodeAlgorithm
 
 #from scipy.spatial import Voronoi as SciVoronoi
 from notifier import Notifier
-from voronoi import Voronoi
+from delaunay import Delaunay
+from point2D import Point2D
 
 
 class DistributedVoronoi(Notifier):
@@ -19,22 +20,24 @@ class DistributedVoronoi(Notifier):
 
         for node in self.network.nodes():
             node.memory['neighbors_data'] = {}
-            node.points = [[int(node.network.pos[node][0]),
-                            int(node.network.pos[node][1])]]
-            node.voronoi = Voronoi(node.points)
-        super(Voronoi, self).initializer()
+            node.points = [(int(node.network.pos[node][0]),
+                            int(node.network.pos[node][1]))]
+            node.voronoi = Delaunay()
+            node.voronoi.add(node.points[0])
+
+        super(DistributedVoronoi, self).initializer()
 
     def step(self, node):
         msg = node.receive()
 
         try:
 
-            if msg.source is None or msg.source != node:
+            if msg is not None and (msg.source is None or msg.source != node):
                 node.memory['neighbors_data'][msg.source.id] = msg.data
                 node.points.append(msg.data)
 
                 # adds new point and recalculates voronoi
                 node.voronoi.add(msg.data)
-        except:
+        except Exception as e:
             raise Exception("Erro during processing message from node %s" 
                             % node)
