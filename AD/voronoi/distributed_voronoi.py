4aeeaf8d643c6e5ca1a6b7fbb8d1e9a35fd07098
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2013-10-16 13:50:01 -0300

    Created the procmsg method
    This method was created to handle the incoming messages

--- AD/voronoi/distributed_voronoi.py
+++ AD/voronoi/distributed_voronoi.py
@@ -5,7 +5,6 @@ from pymote.message import Message
 from pymote.sensor import NeighborsSensor
 from pymote.algorithm import NodeAlgorithm
 
-#from scipy.spatial import Voronoi as SciVoronoi
 from notifier import Notifier
 from delaunay import Delaunay
 from point2D import Point2D
@@ -18,8 +17,10 @@ class DistributedVoronoi(Notifier):
     """
 
     def initializer(self):
+        """ Initialize the host algorithm """
 
         for node in self.network.nodes():
+
             node.memory['neighbors_data'] = {}
             node.points = [(int(node.network.pos[node][0]),
                             int(node.network.pos[node][1]))]
@@ -30,31 +31,40 @@ class DistributedVoronoi(Notifier):
         super(DistributedVoronoi, self).initializer()
 
     def step(self, node):
-        msg = node.receive()
+        """ The step method is called every time a host receives a message """
 
         try:
+            msg = node.receive()
 
             if msg is not None and msg.source != node:
-
-                node.memory['neighbors_data'][msg.source.id] = msg.data
-                node.points.append(msg.data)
-
-                # adds new point and recalculates voronoi
-                node.voronoi.include(msg.data)
-
-                # notifies the node neighbors about the message received
-                self.notify(node, msg.source, msg.data)
+                self.procmsg(node, msg)
 
         except Exception as e:
             raise Exception("Erro during processing message from node %s"
                             % node)
 
+    def procmsg(self, node, msg):
+        """ Process incoming messages """
+
+        # If the message data (host position) is unknown
+        if msg.data not in node.points:
+
+            node.memory['neighbors_data'][msg.source.id] = msg.data
+            node.points.append(msg.data)
+
+            # adds new point and recalculates voronoi
+            node.voronoi.include(msg.data)
+
+            # notifies the node neighbors about the message received
+            self.notify(node, msg.source, msg.data)
+
     def notify(self, node, source, data):
+        """ Notify neighbors. Forwarding operation """
 
         # Starts communication through neighbors nodes
         for neighbor in node.memory[self.neighborsKey]:
-            msg = Message(header=NodeAlgorithm.INI,
-                          source=source,
-                          destination=neighbor,
-                          data=data)
+
+            msg = Message(header=NodeAlgorithm.INI, source=source,
+                          destination=neighbor, data=data)
+
             node.send(msg)
